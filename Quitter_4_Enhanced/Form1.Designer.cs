db9db9defed659a1dd29a09977a5f14e64f7d339
agneovo123 <csenakbaba@gmail.com>
2025-03-06 23:12:02 +0100

    6/9 functionality 0/1 style

--- Quitter_4_Enhanced/Form1.Designer.cs
+++ Quitter_4_Enhanced/Form1.Designer.cs
@@ -29,13 +29,122 @@ namespace Quitter_4_Enhanced
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_suspend = new System.Windows.Forms.Button();
+            this.button_continue = new System.Windows.Forms.Button();
+            this.button_kill = new System.Windows.Forms.Button();
+            this.button_get_networks = new System.Windows.Forms.Button();
+            this.button_ON = new System.Windows.Forms.Button();
+            this.button_OFF = new System.Windows.Forms.Button();
+            this.button_UnegisterHotkey = new System.Windows.Forms.Button();
+            this.button_RegisterHotkey = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "get processes test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_suspend
+            // 
+            this.button_suspend.Location = new System.Drawing.Point(17, 71);
+            this.button_suspend.Name = "button_suspend";
+            this.button_suspend.Size = new System.Drawing.Size(143, 23);
+            this.button_suspend.TabIndex = 1;
+            this.button_suspend.Text = "button_suspend";
+            this.button_suspend.UseVisualStyleBackColor = true;
+            this.button_suspend.Click += new System.EventHandler(this.button_suspend_Click);
+            // 
+            // button_continue
+            // 
+            this.button_continue.Location = new System.Drawing.Point(17, 100);
+            this.button_continue.Name = "button_continue";
+            this.button_continue.Size = new System.Drawing.Size(143, 23);
+            this.button_continue.TabIndex = 2;
+            this.button_continue.Text = "button_continue";
+            this.button_continue.UseVisualStyleBackColor = true;
+            this.button_continue.Click += new System.EventHandler(this.button_continue_Click);
+            // 
+            // button_kill
+            // 
+            this.button_kill.Location = new System.Drawing.Point(17, 129);
+            this.button_kill.Name = "button_kill";
+            this.button_kill.Size = new System.Drawing.Size(143, 23);
+            this.button_kill.TabIndex = 3;
+            this.button_kill.Text = "button_kill";
+            this.button_kill.UseVisualStyleBackColor = true;
+            this.button_kill.Click += new System.EventHandler(this.button_kill_Click);
+            // 
+            // button_get_networks
+            // 
+            this.button_get_networks.Location = new System.Drawing.Point(196, 22);
+            this.button_get_networks.Name = "button_get_networks";
+            this.button_get_networks.Size = new System.Drawing.Size(143, 23);
+            this.button_get_networks.TabIndex = 4;
+            this.button_get_networks.Text = "button_get_networks";
+            this.button_get_networks.UseVisualStyleBackColor = true;
+            this.button_get_networks.Click += new System.EventHandler(this.button_get_networks_Click);
+            // 
+            // button_ON
+            // 
+            this.button_ON.Location = new System.Drawing.Point(196, 100);
+            this.button_ON.Name = "button_ON";
+            this.button_ON.Size = new System.Drawing.Size(143, 23);
+            this.button_ON.TabIndex = 6;
+            this.button_ON.Text = "button_ON";
+            this.button_ON.UseVisualStyleBackColor = true;
+            this.button_ON.Click += new System.EventHandler(this.button_ON_Click);
+            // 
+            // button_OFF
+            // 
+            this.button_OFF.Location = new System.Drawing.Point(196, 71);
+            this.button_OFF.Name = "button_OFF";
+            this.button_OFF.Size = new System.Drawing.Size(143, 23);
+            this.button_OFF.TabIndex = 5;
+            this.button_OFF.Text = "button_OFF";
+            this.button_OFF.UseVisualStyleBackColor = true;
+            this.button_OFF.Click += new System.EventHandler(this.button_OFF_Click);
+            // 
+            // button_UnegisterHotkey
+            // 
+            this.button_UnegisterHotkey.Location = new System.Drawing.Point(373, 100);
+            this.button_UnegisterHotkey.Name = "button_UnegisterHotkey";
+            this.button_UnegisterHotkey.Size = new System.Drawing.Size(143, 23);
+            this.button_UnegisterHotkey.TabIndex = 8;
+            this.button_UnegisterHotkey.Text = "button_UnegisterHotkey";
+            this.button_UnegisterHotkey.UseVisualStyleBackColor = true;
+            this.button_UnegisterHotkey.Click += new System.EventHandler(this.button_UnegisterHotkey_Click);
+            // 
+            // button_RegisterHotkey
+            // 
+            this.button_RegisterHotkey.Location = new System.Drawing.Point(373, 71);
+            this.button_RegisterHotkey.Name = "button_RegisterHotkey";
+            this.button_RegisterHotkey.Size = new System.Drawing.Size(143, 23);
+            this.button_RegisterHotkey.TabIndex = 7;
+            this.button_RegisterHotkey.Text = "button_RegisterHotkey";
+            this.button_RegisterHotkey.UseVisualStyleBackColor = true;
+            this.button_RegisterHotkey.Click += new System.EventHandler(this.button_RegisterHotkey_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(787, 410);
+            this.Controls.Add(this.button_UnegisterHotkey);
+            this.Controls.Add(this.button_RegisterHotkey);
+            this.Controls.Add(this.button_ON);
+            this.Controls.Add(this.button_OFF);
+            this.Controls.Add(this.button_get_networks);
+            this.Controls.Add(this.button_kill);
+            this.Controls.Add(this.button_continue);
+            this.Controls.Add(this.button_suspend);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +152,16 @@ namespace Quitter_4_Enhanced
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_suspend;
+        private System.Windows.Forms.Button button_continue;
+        private System.Windows.Forms.Button button_kill;
+        private System.Windows.Forms.Button button_get_networks;
+        private System.Windows.Forms.Button button_ON;
+        private System.Windows.Forms.Button button_OFF;
+        private System.Windows.Forms.Button button_UnegisterHotkey;
+        private System.Windows.Forms.Button button_RegisterHotkey;
     }
 }
 
--- Quitter_4_Enhanced/Form1.cs
+++ Quitter_4_Enhanced/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using static Quitter_4_Enhanced.ProcessExtension;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 
 namespace Quitter_4_Enhanced
 {
@@ -16,5 +20,130 @@ namespace Quitter_4_Enhanced
         {
             InitializeComponent();
         }
+
+        #region process_stuff
+        Process MyProcess;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process[] processlist = Process.GetProcesses();
+
+
+            foreach (Process process in processlist)
+            {
+                Console.WriteLine("Process: \"{0}\" ID: {1}", process.ProcessName, process.Id);
+                if (process.ProcessName == "notepad++")
+                {
+                    MyProcess = process;
+                    Console.WriteLine("\"notepad++\" FOUND");
+                }
+            }
+        }
+        private void button_suspend_Click(object sender, EventArgs e)
+        {
+            MyProcess.Suspend();
+        }
+
+        private void button_continue_Click(object sender, EventArgs e)
+        {
+            MyProcess.Resume();
+        }
+
+        private void button_kill_Click(object sender, EventArgs e)
+        {
+            MyProcess.Kill();
+        }
+        #endregion
+
+        #region networking_stuff
+        string MyAdapter;
+        private void button_get_networks_Click(object sender, EventArgs e)
+        {
+            List<string> adapters = net_adapters();
+            foreach (string adapter in adapters)
+            {
+                Console.WriteLine("Adapter: \"{0}\"", adapter);
+                if (adapter == "Ethernet 4")
+                {
+                    MyAdapter = adapter;
+                }
+            }
+        }
+        public System.Collections.Generic.List<String> net_adapters()
+        {
+            List<String> values = new List<String>();
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                values.Add(nic.Name);
+            }
+            return values;
+        }
+        private void button_OFF_Click(object sender, EventArgs e)
+        {
+            DisableAdapter(MyAdapter);
+        }
+        private void button_ON_Click(object sender, EventArgs e)
+        {
+            EnableAdapter(MyAdapter);
+        }
+        static void EnableAdapter(string interfaceName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" enable");
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+        static void DisableAdapter(string interfaceName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+        #endregion
+
+        #region hotkey_stuff
+        public int a = 1;
+        // DLL libraries used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        const int MYACTION_HOTKEY_ID = 1;
+
+        private void button_RegisterHotkey_Click(object sender, EventArgs e)
+        {
+            // Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+            // Compute the addition of each combination of the keys you want to be pressed
+            // ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
+            //RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID, 0, (int)Keys.F10);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID, 6, (int)Keys.PageUp);
+
+            //Console.WriteLine(Keys.PageUp);
+            //Console.WriteLine(Keys.Control);
+            //Console.WriteLine(Keys.Shift);
+            //Console.WriteLine(Keys.Alt);
+        }
+
+        private void button_UnegisterHotkey_Click(object sender, EventArgs e)
+        {
+            // Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+            // Compute the addition of each combination of the keys you want to be pressed
+            // ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ID);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID)
+            {
+                a++;
+                MessageBox.Show(a.ToString());
+            }
+            base.WndProc(ref m);
+        }
+
+        #endregion
+
     }
 }
